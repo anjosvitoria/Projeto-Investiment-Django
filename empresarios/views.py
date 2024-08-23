162a05d712f64683b5be904bde48a58ce9f48b25
Vitória Anjos <109820733+anjosvitoria@users.noreply.github.com>
2024-08-23 20:34:11 -0300

    add empresa.html

--- empresarios/views.py
+++ empresarios/views.py
@@ -61,6 +61,9 @@ def listar_empresas(request):
         return render(request, 'listar_empresas.html',{'empresas':empresas})
 
 def empresa(request, id):
+    empresa = Empresas.objects.get(id=id)
+    if request.method == "GET":
+        return render(request, 'empresa.html', {'empresa': empresa})
     
 
 
