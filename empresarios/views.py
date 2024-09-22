883d0c0023da8e6614b2cce4f775589ce1466273
Vitória Anjos <109820733+anjosvitoria@users.noreply.github.com>
2024-09-22 19:51:30 -0300

    Update

--- empresarios/views.py
+++ empresarios/views.py
@@ -72,9 +72,25 @@ def empresa(request, id):
         documentos = Documento.objects.filter(empresa=empresa)
         propostas_investimentos = PropostaInvestimento.objects.filter(empresa = empresa)
         
+        percentual_vendido = 0
+        for pi in propostas_investimentos:
+            if pi.status == 'PA':
+                percentual_vendido= percentual_vendido + pi.percentual   
+        #maneira 2
+        total_captado = sum(propostas_investimentos.filter(status='PA').values_list('valor', flat=True))
+        
+        valuation_atual = (100 * float(total_captado)) / float(percentual_vendido) if percentual_vendido != 0 else 0
+
+                
         proposta_investimentos_enviada = propostas_investimentos.filter(status = 'PE')
-        return render(request, 'empresa.html', {'empresa': empresa, 'documentos': documentos, 'proposta_investimentos_enviada':proposta_investimentos_enviada})
-    
+        return render(request, 'empresa.html', {
+            'empresa': empresa, 
+            'documentos': documentos, 
+            'proposta_investimentos_enviada':proposta_investimentos_enviada, 
+            'percentual_vendido':int(percentual_vendido),
+            'total_captado':total_captado,
+            'valuation_atual':valuation_atual            
+        })
     
 
 def add_doc(request, id):
@@ -128,5 +144,19 @@ def add_metrica(request, id):
     )
     metricas.save()
     
-    messages.add.message(request, constants.SUCESS, "metrica cadastrada com sucesso")
+    messages.add.message(request, constants.SUCcESS, "metrica cadastrada com sucesso")
     return redirect(f'/empresarios/{empresa.id}')
+
+def gerenciar_proposta(request, id):
+    acao = request.GET.get('acao')
+    pi=PropostaInvestimento.objects.get(id=id)
+    
+    if acao == 'aceitar':
+        messages.add_message(request, constants.SUCCESS, 'proposta aceita')
+        pi.status='PA'
+    elif acao == 'recusar':
+        messages.add_message(request, constants.ERROR, 'proposta recusada')
+        pi.status='PR'
+        
+    pi.save()
+    return redirect(f'/empresarios/empresa/{pi.empresa.id}') 
--- investidores/urls.py
+++ investidores/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('sugestao/', views.sugestao, name="sugestao"),
     path('ver_empresa/<int:id>', views.ver_empresa, name="ver_empresa"),
     path('realizar_proposta/<int:id>', views.realizar_proposta, name="realizar_proposta"),
-    path('assinar_contrato/<int:id>', views.assinar_contrato, name="assinar_contrato")
-
+    path('assinar_contrato/<int:id>', views.assinar_contrato, name="assinar_contrato"),
+    path('gerenciar_proposta/<int:id>', views.gerenciar_proposta, name="gerenciar_proposta")
 ]
